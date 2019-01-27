b967de6f8c102720cfe1505094ee3cbaccb8e02a
Samkit Jain <samkit5495@gmail.com>
2019-01-27 21:23:16 +0530

    Code file created.

---
+++ code.py
@@ -0,0 +1,32 @@
+# --------------
+import yaml
+
+# Read the data of the format .yaml type
+with open(path) as f:
+    data = yaml.load(f)
+# Find data type of the file
+print(type(data))
+# In which city, and at which venue the match was played and where was it played ?
+print(data['info']['city'])
+print(data['info']['venue'])
+# Which are all the teams that played in the tournament ? How many teams participated in total?
+print(data['info']['teams'])
+print(len(data['info']['teams']))
+# Which team won the toss and what was the decision of toss winner ?
+print(data['info']['toss']['winner'])
+print(data['info']['toss']['decision'])
+# Find the first bowler and first batsman who played the first ball of the first inning
+print(data['innings'][0]['1st innings']['deliveries'][0][0.1]['bowler'])
+print(data['innings'][0]['1st innings']['deliveries'][0][0.1]['batsman'])
+
+
+# How many deliveries were delivered in first inning ?
+print(len(data['innings'][0]['1st innings']['deliveries']))
+# How many deliveries were delivered in second inning ?
+print(len(data['innings'][1]['2nd innings']['deliveries']))
+
+# Which team won and how ?
+print(data['info']['outcome']['winner'])
+print(data['info']['outcome']['by']['runs'])
+
+
